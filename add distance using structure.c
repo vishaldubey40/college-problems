42aceebf580dcad82aa4b870295867a0be6e7e61
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-07-09 19:21:51 +0530

    Create add distance using structure.c

---
+++ add distance using structure.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+struct distance
+
+{  int a,b,c;
+    /* data */
+}variable;
+
+
+int main(){
+scanf("%d%d",&variable.a,&variable.b);
+variable.c=variable.a+variable.b;
+printf("sum of given to number:%d",variable.c);
+}
