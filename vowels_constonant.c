4032665fa579b0b9baeea12a3256de51bd602199
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-07-15 22:34:24 +0530

    Create vowels_constonant.c

---
+++ vowels_constonant.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    char a,b,c;
+    scanf("%c",&a);
+     printf("Value of a is: %c\n",a);
+     if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
+      printf("given character is a vowel");
+        }else
+     printf(" Given character is consonant");
+}
