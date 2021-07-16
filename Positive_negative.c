5aade8981a40f00c7509d7c7d9539cb429eb5de9
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-07-16 23:50:46 +0530

    Create Positive_negative.c

---
+++ Positive_negative.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+        int n;
+        printf("Enter the number : ");
+        scanf("%d",&n);
+        if(n>0){
+        printf("Positive\n");
+            
+        }else
+        printf("Negative\n");
+    
+}
