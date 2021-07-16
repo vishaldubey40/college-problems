7ab3834f3e1b56b8b832aa6dba7224c9024c552c
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-07-17 00:15:12 +0530

    Create sum_of_natural_numbers.c

---
+++ sum_of_natural_numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+         int n,count=0;
+        printf("Enter the number : ");
+        scanf("%d",&n);
+       for(int i=1;i<=n;i++){
+        count+=i;
+        }        printf("Sum of first n natural number is: %d\n",count);
+            
+        
+    
+}
