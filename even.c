b509acd96ef3b69b3344f2cbfacaae6098127bf8
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-07-21 22:44:19 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int a,b,c;
+    
+     printf("Value of a is: ");
+     scanf("%d",&a);
+     if(a%2==0){
+      printf("given number is an even number");
+        }else
+     printf("Not an even number:");
+}
