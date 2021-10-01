37e86098d437d944ef1853445275690d9dfa135d
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-10-01 23:19:34 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+int billamt,amtgiven;
+
+    scanf("%d%d",&billamt,&amtgiven);
+    
+        printf("Quotient:%d\nRemainder:%d",billamt/amtgiven,billamt%amtgiven);
+    
+return 0;
+}
