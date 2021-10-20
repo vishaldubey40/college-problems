b1afda606579020ea56c77de7399e1044e435092
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-10-20 15:00:19 +0530

    Create increment.cpp

---
+++ increment.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int firstnum,secondnum;
+cin>>firstnum>>secondnum;
+cout<<firstnum--<<" "<<++secondnum<<endl;
+cout<<firstnum++<<" "<<--secondnum<<endl;
+cout<<firstnum<<" "<<++secondnum<<endl;
+cout<<--firstnum<<" "<<--secondnum<<endl;
+cout<<++firstnum<<" "<<++secondnum<<endl;
+	return 0;
+}
