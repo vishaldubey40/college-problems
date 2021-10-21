fdccda330c360d187bbbacf8da054c2e1920d8a0
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-10-21 14:16:41 +0530

    Create moon.cpp

---
+++ moon.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{int weightinearth;
+float weightinmoon;
+
+cin>>weightinearth;
+ weightinmoon=(weightinearth/9.8)*1.6268;
+ cout<<weightinmoon;
+
+
+
+	return 0;
+}
