e886ad65429a04ad503d51fa74321e2d4f805a81
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-11-10 10:51:45 +0530

    Create data.cpp

---
+++ data.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define tt for(int i=0;i<t1-1;i=i+2) 
+#define p for(int i=0;i<n-1;i=i+2)
+#define p1 if(n%2==0)
+#define t1 n-3
+#define z else
+#define r cout<<s[t1]<<s[t1+1]<<s[t1+2]
+#define p2 cout<<s[i]<<s[i+1]
+//#define
+using namespace std;
+class ContactNumbers
+{public:int Phone(int n){
+string s;
+cin>>s;
+p1{
+p{p2;
+if(i!=n-2){
+cout<<"-";}
+}}z
+{tt p2<<"-";
+r;
+}return 0;}};
+int main()
+{ContactNumbers Digits;
+int n;cin>>n;Digits.Phone(n);
+return 0;
+}
