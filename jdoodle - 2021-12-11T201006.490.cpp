affb97c2cd66707c49e72494ee29a9e3f4c77c79
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-12-13 20:49:36 +0530

    Add files via upload

---
+++ jdoodle - 2021-12-11T201006.490.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a;
+        cin>>a;
+        int ar[a];
+        cout<<a<<" ";
+        for(int i=1;i<a;i++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        
+    }
+}
