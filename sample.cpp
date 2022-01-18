63a3df03b9b6ee17954ede9f9bd3c10be03abebd
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2022-01-18 21:04:29 +0530

    Add files via upload

--- sample.cpp
+++ sample.cpp
@@ -1,37 +1,55 @@
-#include<bits/stdc++.h>
-//#define ll long long 
-
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define pi (3.141592653589)
+#define mod 1000000007
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
-void print(vector<int> &v){
-    for(int i=0;i<v.size();i++){
-        cout<<v[i]<<" ";
-    }cout<<endl;
+
+bool isPrime(int n){
+if(n==1) return false;
+if(n==2) return true;
+for(int i=2;i*i<=n;i++){
+if(n%i==0)return false;
+}
+return true;
 }
 
-int main(){
-    int n;
-    cin>>n;
-    vector<int> v[n];
-    for(int i=0;i<n;i++){
-        int k;
-        cin>>k;
-        for(int j=0;j<n;j++){
-            int x;
-            cin>>x;
-            v[i].push_back(x);
-        }
+int32_t main(){
+fast
+
+
+// Fuck Ratings, I'm in Love with Experience.
+// Once a Charas, Always a CHARAS.
+
+
+int t=1;
+cin>>t;
+int a[t];
+for(int i=0;i<t;i++){
+    cin>>a[i];
+}
+int j=t;
+       int k=1;
+while(j--){
+for(int i=0;i<t;i++){
+    if(a[i]==k){
+        cout<<i+1<<" ";
+        k++;
     }
- for(int i=0;i<n;i++){
-    print(v[i]);
- }
-    // vector<pair<int,int>> v;
-      //for(int i=0;i<4;i++){
-        // int x,y;
-         //cin>>x>>y;
-         //v.push_back({x,y});
-    // }
-     //for(int i=0;i<v.size();i++){
-      ///   cout<<v[i].first<<" "<<v[i].second<<endl;
-     //}
+}     
+}
+
+return 0;
 }
