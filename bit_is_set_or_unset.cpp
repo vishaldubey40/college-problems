d06262b0fae466e6c337c1f4f5d7ffa66294444f
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2022-02-03 22:34:02 +0530

    Create bit_is_set_or_unset.cpp

---
+++ bit_is_set_or_unset.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a=9;
+    int i;
+    cin >> i;
+    if( (a & (1 << i)) != 0){
+        cout << "bit is set "<<endl;
+    }else cout << " bit is unset" <<endl;
+
+  //  cout<<"Sum of x+y = " << z;
+}
