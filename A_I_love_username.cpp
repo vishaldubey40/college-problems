dc2033ea6f909bee8222a63c2763460e3f1c994a
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2022-03-31 17:18:40 +0530

    Add files via upload

---
+++ A_I_love_username.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pi (3.141592653589)
+#define mod 1000000007
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+
+bool isPrime(int n){
+if(n==1) return false;
+if(n==2) return true;
+for(int i=2;i*i<=n;i++){
+if(n%i==0)return false;
+}
+return true;
+}
+
+int32_t main(){
+fast
+
+
+// Fuck Ratings, I'm in Love with Experience.
+// Once a Charas, Always a CHARAS.
+
+
+int t=1;
+cin>>t;
+int arr[t];
+for(int i=0;i<t;i++)
+cin>>arr[i];
+int worst = arr[0],best = arr[0];
+int cnt = 0;
+for(int i=1;i<t;i++)
+{
+  if(arr[i]<worst){
+      cnt++;
+      worst=arr[i];
+  }else if(arr[i]>best){
+      cnt++;
+      best=arr[i];
+  }
+}
+cout << cnt <<endl;
+return 0;
+}
